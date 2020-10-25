ed16837d84e85cd93e95d6c2d9996336012a6ddd
CaedenZ <fengye16@ichat.sp.edu.sg>
2020-10-25 18:27:59 +0800

    update_comment

--- bfs2.py
+++ bfs2.py
@@ -2,77 +2,108 @@ from queue import Queue
 from _collections import defaultdict
 
 #-------- To obtain shortest path --------#
+
+
 def getResult(graph, size, src_node, hospitals, k):
-    parent = [-1] * size                                                    #Set the starting node size
-    nearest_hospitals = BFS(graph, size, src_node, hospitals, parent, k)    # to call the BFS 
-    paths = []                                                              # create an array for the shortest path
-    
-    for current_hospital in nearest_hospitals:                              
+    parent = [-1] * size  # Init the path tracing array
+    nearest_hospitals = BFS(graph, size, src_node,
+                            hospitals, parent, k)    # to call the BFS
+    # create an array for the shortest path
+    paths = []
+
+    for current_hospital in nearest_hospitals:
         current = current_hospital
         path = [current]
-        while parent[current] != -1:                                        # When parent[current] not equals to -1, there is a neighbour present
-            path.append(parent[current])                                    # The neighbour will be added to the path
-            current = parent[current]                                       # The neighbour will now become the current node
-        paths.append(path.copy())                                           # Update the path into paths
+        # When parent[current] not equals to -1, it have not reach the src node
+        while parent[current] != -1:
+            # The parent will be added to the path
+            path.append(parent[current])
+            # The parent will now become the current node
+            current = parent[current]
+        # Append the path into paths
+        paths.append(path.copy())
     return paths
 
 #------------ BFS code -------------#
+
+
 def BFS(graph, size, src_node, hospitals, parent, k):
-    queue = Queue()                                                         # Start a queue
-    visited = [False] * size                                                # Set every single node to not visited
-    tmp_k = 0                                                               # Number of hospitals found
-    hospitalList = []                                                       # Create a list for hospitals
-    visited[src_node] = True                                                # Set starting node as visited
-    queue.EnQueue(src_node)                                                 # Add starting node to the queue
+    # Start a queue
+    queue = Queue()
+    # Set every single node to not visited
+    visited = [False] * size
+    # Number of hospitals found
+    tmp_k = 0
+    # Create a list for hospitals
+    hospitalList = []
+    # Set starting node as visited
+    visited[src_node] = True
+    # Add starting node to the queue
+    queue.EnQueue(src_node)
 
-    if str(src_node) in hospitals:                                          # If starting node is a hospital, 
-        tmp_k += 1                                                          # Add 1 to hospitals found
-        hospitalList.append(src_node)                                       # Add source node to hospital list
+    # If starting node is a hospital,
+    if str(src_node) in hospitals:
+        # Add 1 to hospitals found
+        tmp_k += 1
+        # Add source node to hospital list
+        hospitalList.append(src_node)
 
-    # Hello Caeden please update the explanation for this
-    # parent = [-1, -1, -1]
-    # if current = 3
-    # parent[current] = 1
-    # path.append(1)
-    # current = parent[current] = 1
-    # updated path: [-1, 0, 0, 1]  
-    # -1: Not Visited 
-    # 0: Neighbour 
-    # 1: Visted
-    # parent = [-1, -1, 1]
+    # parent = [-1, 2 ,0 ,1]
+    # if current = hopital_node  = 1
+    # parent[current] = 2
+    # path.append(2)
+    # current = parent[current] = 2
+    # parent[current] = 0
+    # path.append(0)
+    # current = parent[current] = 0
+    # parent[current] = -1
+    # path = [2, 0]
+    # path to hospital 1 from node 0 is [0, 2, 1]
 
-    while not queue.isEmpty() and tmp_k < k:                                # When there is something in the queue and number of hospitals found is less than k,
-        current_node = queue.DeQueue()                                      # Get current node from the queue
-        for nextNode in graph[current_node]:                                # Get the next node base on the graph
-            if not visited[nextNode]:                                       # Checks if the node is visited before or not, if not visited,
-                visited[nextNode] = True                                    # Next node will be marked as visited
-                parent[nextNode] = current_node                             # Set the neighbour as the current node
-                queue.EnQueue(nextNode)                                     # Add next node to the queue
-                if str(nextNode) in hospitals:                              # if next node is a hospital,
+    # When there is something in the queue and number of hospitals found is less than k,
+    while not queue.isEmpty() and tmp_k < k:
+        # Get current node from the queue
+        current_node = queue.DeQueue()
+        # Get the next node base on the graph
+        for nextNode in graph[current_node]:
+            # Checks if the node is visited before or not, if not visited,
+            if not visited[nextNode]:
+                # Next node will be marked as visited
+                visited[nextNode] = True
+                # Set the neighbour as the current node
+                parent[nextNode] = current_node
+                # Add next node to the queue
+                queue.EnQueue(nextNode)
+                # if next node is a hospital,
+                if str(nextNode) in hospitals:
                     tmp_k += 1                                              # Add 1 to hospitals found
-                    hospitalList.append(nextNode)                           # Add source node to hospital list
-                    if tmp_k >= k:                                          # Check if number of hospitals is more than k 
-                        break                                               
+                    # Add source node to hospital list
+                    hospitalList.append(nextNode)
+                    if tmp_k >= k:                                          # Check if number of hospitals is more than k
+                        break
     return hospitalList
 
 
 if __name__ == '__main__':
 
-    g = defaultdict(list)                                                   # create a dictionary list for our graph
+    # create a dictionary list for our graph
+    g = defaultdict(list)
 #-------- Prepare the data --------#
     edgeFile = "graphs.txt"
     hospitalFile = "hospital2.txt"
     edgeFileOpen = open(edgeFile, "r")
-    clearStr = edgeFileOpen.readline()                                      
-    while clearStr[0] == '#':                                               # Remove strings before the data
+    clearStr = edgeFileOpen.readline()
+    # Remove strings before the data
+    while clearStr[0] == '#':
         clearStr = edgeFileOpen.readline()
-    str1 = clearStr + edgeFileOpen.read()                                   
+    str1 = clearStr + edgeFileOpen.read()
     edgeFileOpen.close()
     arr1 = str1.split("\n")
 
     size = 0
 
-    for i in arr1[0:-1]:                                                    # Input our data into our graph in a matrix format
+    # Input our data into our graph in a matrix format
+    for i in arr1[0:-1]:
         a = i.split("\t")
         g[int(a[0])].append(int(a[1]))
         if int(a[0]) > size:
@@ -89,17 +120,21 @@ if __name__ == '__main__':
     hospitals = str2.split("\n")
     hospitalFileOpen.close()
 
-    k = int(input("Enter value for k (top-k hospital): "))                                   # Get input k from user (top-k hospital)
+    # Get input k from user (top-k hospital)
+    k = int(input("Enter value for k (top-k hospital): "))
 
 #------- To retrieve the result --------#
     result = open("result.txt", 'w')
 
     for node in sorted(g.keys()):
-        paths = getResult(g, size, node, hospitals, k)                                       # Call the shortest path function
-        result.write('source node  :' + str(node) + ':  ')                                    
-        for path in paths:                                                                   # Output every path into the text file
+        # Call the shortest path function
+        paths = getResult(g, size, node, hospitals, k)
+        result.write('source node  :' + str(node) + ':  ')
+        # Output every path into the text file
+        for path in paths:
             result.write(', distance: ' + str(len(path) - 1) + ',  path: [')
-            string = ', '.join(str(x) for x in path[::-1])                                   # Reverse the path to ensure result starts from the source node and ends at hospital
+            # Reverse the path to ensure result starts from the source node and ends at hospital
+            string = ', '.join(str(x) for x in path[::-1])
             result.write(string + ' ]')
 
         result.write('\n')
